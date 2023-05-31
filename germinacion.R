ffb569623ffdb5f47220dafaf07f2b88bd846c98
kathiavillarreal <124898323+kathiavillarreal@users.noreply.github.com>
2023-05-31 15:40:36 -0600

    Add files via upload

---
+++ germinacion.R
@@ -0,0 +1,50 @@
+# KMVA
+# 25/05/2023
+
+
+# Importar datos ----------------------------------------------------------------------
+
+Germ <- read.csv("SCIPS/Germinacion.Csv", header = T)
+Germ$Trat <- as.factor(Germ$Trat)
+
+
+# Grafica ----------------------------------------------------------------------------
+
+boxplot(Germ$PG ~ Germ$Trat,
+        col="PINK",
+        xlab = "Tratamientos Germinativos",
+        ylab = "% Germinacion")
+# ordenar tratamientos
+levels(Germ$Trat)
+levels(Germ$Trat)
+boxplot(Germ$PG ~ Germ$Trat,
+        col="PINK")
+# funcion tapply
+tapply(Germ$PG, Germ$Trat, mean)
+tapply(Germ$PG, Germ$Trat, var)
+
+# ANOVA -----------------------------------------------------------------------------
+
+
+
+Germ.oav <- aov(Germ$PG ~ Germ$Trat)
+summary(Germ.oav)
+
+
+# prueba de Tuckey ------------------------------------------------------------------
+
+TukeyHSD(Germ.oav, conf.level = 0.95)
+
+plot(TukeyHSD(Germ.oav), las =1, col="red")
+
+boxplot(Germ$PG ~ Germ$Trat,
+        col="PINK",
+        xlab = "Tratamientos Germinativos",
+        ylab = "% Germinacion",
+        ylim=c(0,10))
+text(1,4.3, "a", col="red")
+text(2, 8.3, "b", col="blue")
+text(3, 9.3, "b", col="blue")
+text(4, 7.3, "b", col="blue")
+text(5, 3.3, "a", col="red")
+getwd()
---
+++ laboratorio_01.R
@@ -0,0 +1,3 @@
+#kathia marisol villarreal arriaga
+#23/02/2023
+#laboratorio 1
